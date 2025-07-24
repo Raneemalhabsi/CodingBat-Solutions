f699b629188d42242a901a24873c59cf10cc0d87
Raneem <raneemalhabsi02@gmail.com>
2025-07-24 15:17:08 +0400

    Added nearHundered.java

---
+++ Warmup-1/nearHundered.java
@@ -0,0 +1,9 @@
+public class nearHundered {
+    public boolean nearHundred(int n) {
+        if (Math.abs(n-100) <= 10 || Math.abs(n-200) <=10){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
