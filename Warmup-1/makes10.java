dc253420b914acb0f526d34c283fa2908255c558
Raneem <raneemalhabsi02@gmail.com>
2025-07-26 18:36:29 +0400

    Added makes10.java

---
+++ Warmup-1/makes10.java
@@ -0,0 +1,10 @@
+public boolean makes10(int a, int b) {
+    if (a==10 ){
+        return true;
+    }else if(b==10){
+        return true;
+    }
+    else if(a+b==10){
+        return true;
+    }return false;
+}
