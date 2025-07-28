f5761837e1cd963c2653adea87f585b6040e9818
Raneem <raneemalhabsi02@gmail.com>
2025-07-28 12:32:38 +0400

    Added allSwap.java

---
+++ allSwap.java
@@ -0,0 +1,19 @@
+public String[] allSwap(String[] strings) {
+    Map <String, Integer> map = new HashMap <String, Integer>();
+    for (int i =0 ; i<strings.length; i++){
+        String firstChar = strings[i].substring(0,1);
+        if (map.containsKey(firstChar)){
+            int value = map.get(firstChar);
+            if (value != -1){
+                String newStr = strings[i];
+                strings[i] = strings[value];
+                strings[value] = newStr;
+
+                map.remove(firstChar);
+            }
+        }else{
+            map.put(firstChar, i);
+        }
+    }return strings;
+}
+
