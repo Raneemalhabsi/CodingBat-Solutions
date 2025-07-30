1dddcef8332f83687beefb51419a8fe2538b619a
Raneem <raneemalhabsi02@gmail.com>
2025-07-30 14:27:48 +0400

    Added Array-2/ twoTwo.java

---
+++ Array-2/twoTwo.java
@@ -0,0 +1,9 @@
+public boolean twoTwo(int[] nums) {
+    for (int i = 0; i<nums.length; i++){
+        if (nums[i] == 2){
+            if (!(i>0 && nums[i-1] == 2) && !( i<nums.length-1 && nums[i+1]==2)){
+                return false;
+            }
+        }
+    }return true;
+}
