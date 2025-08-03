1d0536fa68df24e38144af367a8acebef8bcc2fc
Raneem <raneemalhabsi02@gmail.com>
2025-08-03 12:56:53 +0400

    Added Recursion-2/ splitArray.java

---
+++ Recursion-2/splitArray.java
@@ -0,0 +1,18 @@
+public boolean splitArray(int[] nums) {
+    return helpSplit(0, nums,0,0);
+}
+public boolean helpSplit(int start, int[] nums, int sum1, int sum2){
+    if (start >= nums.length){
+        if (sum1 == sum2){
+            return true;
+        }else{
+            return false;
+        }
+    }if (helpSplit(start +1, nums, sum1 + nums[start], sum2) ){
+        return true;
+    }if (helpSplit(start +1, nums, sum1, sum2  + nums[start]) ){
+        return true;
+    }else{
+        return false;
+    }
+}
